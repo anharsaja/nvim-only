b18b3887a078bbedc20cd118fadc2209ac192a6a
anharsaja <anharmuk12@gmail.com>
2024-12-01 01:41:05 +0000

    feat: asutosave

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,4 +3,11 @@ return {
     "jwalton512/vim-blade",
     ft = "blade",
   },
+  {
+    "neoclide/coc.nvim",
+    config = function()
+      -- Konfigurasi untuk integrasi dengan Intelephense
+      vim.g.coc_global_extensions = { "coc-phpls" }
+    end,
+  },
 }
