144dfa95d070237fa9bc709ba0fdef368ec93fca
anharsaja <anharmuk12@gmail.com>
2024-12-01 01:52:01 +0000

    feat: delete fzf

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -3,11 +3,4 @@ return {
     "jwalton512/vim-blade",
     ft = "blade",
   },
-  {
-    "neoclide/coc.nvim",
-    config = function()
-      -- Konfigurasi untuk integrasi dengan Intelephense
-      vim.g.coc_global_extensions = { "coc-phpls" }
-    end,
-  },
 }
